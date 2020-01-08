9d8286376d556e33a25c43271cf3ad4e4eeed9e8
杨立新 <yanglx90@chinaunicom.cn>
2020-01-08 23:39:32 +0800

    通过dva实现

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,14 +1,32 @@
-import React, {useState} from 'react';
+import React, { useCallback, useEffect } from 'react';
+import { connect } from 'dva';
+
 
 function Home(props) {
-
-  const [count,setCount]=useState(0);
-
+  const ussTest=useCallback(()=> {
+    props.dispatch(
+      {
+        type: 'homeModel/add',
+        payload:{count:122}
+      }
+    );
+  })
+  useEffect(
+    ()=> {
+      ussTest();
+      return ()=>console.log('结束')
+    },[props.homeModel.loading, ussTest]
+  )
 
 
   return (
-    <div><button onClick={()=>setCount(count+1)}>Click me</button></div>
+    <div>
+      {/*<button onClick={() => props.home.add(count + 1)}>Click me</button>*/}
+      <p>{props.homeModel.count}</p>
+      <button onClick={ ussTest}>Click me</button>
+    </div>
   );
 }
 
-export default Home;
+export default connect(({homeModel}) => ({homeModel}))(Home);
+
